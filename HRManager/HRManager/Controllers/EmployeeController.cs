859a13478e76291360d02480e145b1fbd41e5e12
ryscottarmstrong <ryscottarmstrong@gmail.com>
2019-09-21 23:04:17 -0600

    Created an EmployeeController with a AllEmployees method and subsequent AllEmployees method. Receiving error message "The localhost page can't be found" Why is the program searching for Employee/Employee?

---
+++ HRManager/HRManager/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRManager.Models;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRManager.Controllers
+{
+    public class EmployeeController: Controller
+    {
+        public IActionResult AllEmployees()
+        {
+            Employee employee = new Employee()
+            {
+                FirstName = "Ryan",
+                LastName = "Armstrong",
+                Address = "837 E 2600 N North Ogden, UT 84414",
+                Email = "ryscottarmstrong@gmail.com",
+                PhoneNumber = "801-900-5981",
+                Position = "Software Developer",
+                // How do I instantiate a Dictionary variable?
+                //Department = <1, "Software Development"
+                // DateTime StartDate { get; set; }
+                // DateTime EndDate { get; set; }
+                // Dictionary<int, string> EmploymentStatus { get; set; }
+                // Dictionary<int, string> Shift { get; set; }
+                // Employee Manager { get; set; }
+                ////Team Member Photo (above my skill level at present)
+                FavoriteColor = "I like colors"
+            };
+            return View();
+        }
+    }
+}
---
+++ HRManager/HRManager/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRManager.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Position { get; set; }
+        public Dictionary<int, string> Department { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Dictionary<int, string> EmploymentStatus { get; set; }
+        public Dictionary<int, string> Shift { get; set; }
+        public Employee Manager { get; set; }
+        //Team Member Photo (above my skill level at present)
+        public string FavoriteColor { get; set; }
+    }
+}
