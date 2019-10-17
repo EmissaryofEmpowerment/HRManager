9b7dd21f18d09ab1228eefe97335f1e95e6d4ec5
ryscottarmstrong <ryscottarmstrong@gmail.com>
2019-10-17 09:03:01 -0600

    Created a View called CreateRecord. Added anchor tags to Individual Team Member View:to Create new record, edit an existing record, or delte an existing record

--- HRManager/HRManager/Controllers/EmployeeController.cs
+++ HRManager/HRManager/Controllers/EmployeeController.cs
@@ -11,7 +11,7 @@ namespace HRManager.Controllers
 {
     public class EmployeeController: Controller
     {
-        public IActionResult AllEmployees(int? EmployeeID, string sortBy)
+        public IActionResult AllEmployees()
         {
 
             Employee employee = new Employee()
@@ -69,13 +69,52 @@ namespace HRManager.Controllers
                        }
 
             };
+            Employee _4th_employee = new Employee()
+            {
+                FirstName = "Via",
+                LastName = "Armstrong",
+                FavoriteColor = "red",
+            };
             List<Employee> pointer = new List<Employee>()
-            { _2ndEmployee, employee, Lots_of_Cheese, _3rd_employee };
+            { _2ndEmployee, employee, Lots_of_Cheese, _3rd_employee, _4th_employee };
             HRManager.Models.ViewModels.List_Employee_ViewModel
                 tester_view_model = new List_Employee_ViewModel();
             tester_view_model.Employee_List = pointer;
 
               return View(tester_view_model);
+        }
+        public IActionResult IndividualTeamMember()
+        {
+            Employee individual = new Employee()
+            {
+                EmployeeId = 1,
+                FirstName = "Ryan",
+                LastName = "Armstrong",
+                Address = "837 E 2600 N North Ogden, UT 84414",
+                Email = "ryscottarmstrong@gmail.com",
+                PhoneNumber = "801-900-5981",
+                Position = "Software Developer",
+                Department = HRManager.Models.Constants.Department.Information_Technology,
+                StartDate = new DateTime(2017, 04, 03),
+                EndDate = new DateTime(),
+                EmploymentStatus = HRManager.Models.Constants.Employment_Status.Owner,
+                Shift = HRManager.Models.Constants.Shift.Day,
+                Manager = new Employee()
+                {
+                    FirstName = "Heavenly",
+                    LastName = "Father",
+                },
+                ////Team Member Photo (above my skill level at present)
+                FavoriteColor = "I like colors",
+                PermissionLevel = HRManager.Models.Constants.PermissionLevel.Low,
+                UserName = "RyScott",
+                PassWord = "Uint%43K8"
+            };
+            return View(individual);
+        }
+        public IActionResult CreateRecord()
+        {
+            return View();
         }
     }
 }
