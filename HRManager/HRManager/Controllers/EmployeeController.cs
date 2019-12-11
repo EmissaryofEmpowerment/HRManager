8736147a69634976bfd965b1997a934903707380
ryscottarmstrong <ryscottarmstrong@gmail.com>
2019-12-11 08:55:10 -0700

    Removed a few anchor elements that linked to personal work and inserted the respective links in the copyright element in _Layout.cshtml Also, added specific instances of Employee.cs to the EmployeeController.cs

--- HRManager/HRManager/Controllers/EmployeeController.cs
+++ HRManager/HRManager/Controllers/EmployeeController.cs
@@ -21,26 +21,70 @@ namespace HRManager.Controllers
         {
             EmployeeViewModel EmpList = new EmployeeViewModel();
             List<Employee> pointer = new List<Employee>();
+            Employee FirstPerson = new Employee()
+            {
+                FirstName = "Gustav",
+                LastName = "Heinmarsh",
+                Address = "45728 Marshall Lane Bentonville, AK ",
+                Department = Constants.Department.Distribution,
+               Shift = Constants.Shift.Day
+            };
+            Employee SecondPerson = new Employee()
+            { FirstName = "Matoape",
+                LastName = "Fengitoa",
+                Address = "898 Cedar Grove Blvd, Portland, ME",
+                Department = Constants.Department.Distribution,
+                Shift = Constants.Shift.Day,
+                FavoriteColor = "White",
+                Manager = FirstPerson
+            };
+            Employee ThirdPerson = new Employee()
+            {
+                FirstName = "Li",
+                LastName = "Chen",
+                Address = "7882 Blue Lake Rd, Boulder, CO",
+                Department = Constants.Department.Service,
+                Shift = Constants.Shift.Night,
+                FavoriteColor = "Red",
+            };
+            Employee FourthPerson = new Employee()
+            {
+                FirstName = "Robert",
+                LastName = "Mbuntu",
+                Address = "3499 Crescent Moon Dr, Lincoln, NE",
+                Department = Constants.Department.Service,
+                Shift = Constants.Shift.Day,
+                Manager = ThirdPerson
+            };
+            pointer.Add(FirstPerson);
+            pointer.Add(SecondPerson);
+            pointer.Add(ThirdPerson);
+            pointer.Add(FourthPerson);
+
             EmpList.Employee_List = pointer;
               return View(EmpList);
         }
         public IActionResult IndividualTeamMember()
         {
-            Employee individual = new Employee();
+            Employee individual = new Employee()
+            {
+                FirstName = "Pierre",
+                LastName = "Roucheau",
+            };
             return View(individual);
         }
         
-        public IActionResult CreateRecord(int id = 0)
-        {
-            if (id==0)
-            {
-                return View(new Employee());
-            }
-            else
-            {
-                return View(_contextDB.Employees);
-            }
-        }
+        //public IActionResult CreateRecord(int id = 0)
+        //{
+        //    if (id==0)
+        //    {
+        //        return View(new Employee());
+        //    }
+        //    else
+        //    {
+        //        return View(_contextDB.Employees);
+        //    }
+        //}
 
         [HttpPost]
         [ValidateAntiForgeryToken]
